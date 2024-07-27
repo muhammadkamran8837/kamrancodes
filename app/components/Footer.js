d263a5e2791f3b09ec64b2aeacb6154463ea2873
Muhammad Kamran <kamrankhanmashwani@gmail.com>
2024-07-27 08:29:12 +0500

    New change

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -7,11 +7,11 @@ export default function Footer() {
       className=" lg:h-[350px] bg-slate-900 mt-20 text-neutral-200 pt-10 rounded-tl-full rounded-tr-full justify-between"
     >
       <div className="flex items-center justify-center flex-col gap-5">
-        <h1 className="font-extrabold text-3xl tracking-wide">LET'S TALK</h1>
+        <h1 className="font-extrabold text-3xl tracking-wide">LETS TALK</h1>
         <h1>kamrankhanmashwani@gmail.com</h1>
       </div>
       <div className="flex items-center justify-center  gap-5 mt-20 flex-col lg:flex-row">
-        <p className="text-xs">Copyrights © 2024 – Kamran Codes</p>
+        <p className="text-xs">Copyrights © 2024 - Kamran Codes</p>
         <button className=" bg-blue-800 text-white rounded-2xl shadow-sm shadow-slate-600 px-3 py-1 w-[150px] ">
           <a href="https://github.com/muhammadkamran8837"> Github</a>
         </button>
