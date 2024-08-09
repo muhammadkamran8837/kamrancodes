f20634a465ba9016fee4dffdce4746c900425146
Muhammad Kamran <kamrankhanmashwani@gmail.com>
2024-08-09 21:35:27 +0500

    Bugs fixing

--- app/projects/app_development/ArtifyApp.jsx
+++ app/projects/app_development/ArtifyApp.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React, { Suspense } from "react";
+import React from "react";
 
 const screenshots = [
   "/artify0.jpeg",
@@ -29,23 +29,21 @@ const screenshots = [
 
 export default function ArtifyApp() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-        {screenshots.map((src, index) => (
-          <div
-            key={index}
-            className="rounded overflow-hidden shadow-lg transition-opacity duration-300"
-          >
-            <Image
-              width={400}
-              height={500}
-              src={src}
-              className="w-full h-full object-cover opacity-50 hover:opacity-100 transition-opacity duration-300"
-              alt=""
-            />
-          </div>
-        ))}
-      </div>
-    </Suspense>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+      {screenshots.map((src, index) => (
+        <div
+          key={index}
+          className="rounded overflow-hidden shadow-lg transition-opacity duration-300"
+        >
+          <Image
+            width={400}
+            height={500}
+            src={src}
+            className="w-full h-full object-cover opacity-50 hover:opacity-100 transition-opacity duration-300"
+            alt=""
+          />
+        </div>
+      ))}
+    </div>
   );
 }
