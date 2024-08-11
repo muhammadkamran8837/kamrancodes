e556a16716603ccc1936d5bbfc33e464c9b54eba
Muhammad Kamran <kamrankhanmashwani@gmail.com>
2024-08-11 21:16:34 +0500

    Changes

--- app/components/HeroSection.js
+++ app/components/HeroSection.js
@@ -9,18 +9,14 @@ export default function HeroSection() {
       <button className="mb-5 bg-blue-800 text-white rounded-2xl shadow-md shadow-neutral-200 px-2 py-1 ">
         Open to Projects 🚀
       </button>
-      <h1 className="lg:text-5xl text-3xl font-black mb-4 text-center lg:w-[64vw] ">
-        Transforming Ideas into Reality,
-        <span className="text-blue-800">
-          Your Go-To Expert for Web and App Development
-        </span>
+      <h1 className="lg:text-5xl text-3xl font-extrabold tracking-wide mb-4 text-center lg:w-[64vw] ">
+        Crafting Digital Excellence:
+        <span className="text-blue-800">Web, App, and WordPress Solutions</span>
       </h1>
-      <p className="text-center px-8 max-w-2xl">
-        Your tech partner: specialize in creating responsive, high-performance
-        websites and applications tailored to meet your business needs.My
-        expertise spans across frontend development with React and Next.js,
-        backend development with Node.js and Express.js, and complete e-commerce
-        solutions.
+      <p className="text-center  px-8 max-w-2xl">
+        Your Tech Partner: Offering Expertise in Web Development, Mobile App
+        Development, and WordPress using Modern Technologies including MERN,
+        Next.js, React Native, Firebase, and FastAPI.
       </p>
     </div>
   );
